b4f744ab3308838f12d7f54fbcd209696afd319d
skiran017 <skiran017@gmail.com>
2023-08-29 12:19:21 +0530

    feat: add input and code elements, add dynamic run command

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { WebContainer } from '@webcontainer/api'
 
 function reportOutput(output: string) {
-  console.log(output)
+
+  outputPanel.textContent += '\n' + (output);
 }
 
 window.addEventListener("load", async (
@@ -9,4 +10,24 @@ window.addEventListener("load", async (
   reportOutput("Booting...")
   const wc = await WebContainer.boot()
   reportOutput("Booting complete!")
+
+  const runCommand = async (cmd: string, args: []) => {
+    const process = await wc.spawn(cmd, args)
+
+    //stream output
+    process.output.pipeTo(new WritableStream({
+      write: (chunk) => {
+        reportOutput(`Process output: ${chunk}`)
+      }
+    }))
+
+    if (await process.exit) {
+      reportOutput(`Process failed and exited with code ${process.exit}`)
+    }
+  }
+
+
+
+
+
 })
