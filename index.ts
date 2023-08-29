ee09980e5646d9553dd54df0c1bcb234e5e54940
skiran017 <skiran017@gmail.com>
2023-08-29 11:55:10 +0530

    feat: boot web container

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hiii")
+import { WebContainer } from '@webcontainer/api'
+
+function reportOutput(output: string) {
+  console.log(output)
+}
+
+window.addEventListener("load", async (
+) => {
+  reportOutput("Booting...")
+  const wc = await WebContainer.boot()
+  reportOutput("Booting complete!")
+})
